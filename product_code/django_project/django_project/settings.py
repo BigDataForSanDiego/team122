7d3d82afa9da751a1917f67dd4caa187b5da2174
jaxonhill <jaxoncharleshill@gmail.com>
2022-10-11 14:12:50 -0700

    Create serializers

--- product_code/django_project/django_project/settings.py
+++ product_code/django_project/django_project/settings.py
@@ -42,11 +42,15 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Local
     "info.apps.InfoConfig",
+    # 3rd Party
+    "corsheaders",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -126,3 +130,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# CORS
+CORS_ORIGIN_WHITELIST = ("http://localhost:3000",)
